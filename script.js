6c08bf1f43c33215550a04a2d32017a65794a60e
Tobias Dreifke <toobsecret@gmail.com>
2025-07-07 12:30:55 +0200

    fetched the names of first generation pokemon and rendered in innerHTML

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-// Initialization function called when script runs
 function init() {
     myRenderFunction();
 }
 
-// Example render or main logic function
-function myRenderFunction() {
-    // TODO: Add your rendering or app logic here
+async function fetchPokemonFirstGeneration() {
+    try {
+        let responseRef = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151");
+        let response = await responseRef.json();
+        return response.results || response;
+
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+};
+
+async function myRenderFunction() {
+    let pokemonFirstGeneration = await fetchPokemonFirstGeneration();
     console.log("Render function called");
+    console.log(pokemonFirstGeneration);
     let contentRef = document.getElementById("content");
-    contentRef.innerHTML = getMyRenderTemplate ();
-}
+    for (let index = 0; index <= pokemonFirstGeneration.length; index++) {
+        console.log(pokemonFirstGeneration[index]);
+        contentRef.innerHTML += getMainPokedexTemplate(pokemonFirstGeneration[index].name);
+        
+    }
 
-function getMyRenderTemplate () {
-    return `<div>HEY</div>`
-}
+};
 
-// Kick off the app
+function getMainPokedexTemplate(name) {
+    return `<div>${name}</div>`
+};
+
 init();
+
+
+
+
+
+
+
+// POKEDEX API URL https://pokeapi.co/api/v2/pokemon?offset=0&limit=5
