f46e67af7f71162895769616c323cd8eef6d8b68
Tobias Dreifke <toobsecret@gmail.com>
2025-07-07 13:09:50 +0200

    fetched types and rendered the BG corresponsing to type

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function init() {
 
 async function fetchPokemonFirstGeneration() {
     try {
-        let responseRef = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151");
-        let response = await responseRef.json();
-        return response.results || response;
-
+        // let responseRef = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151");
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10");
+        let responseData = await response.json();
+        return responseData.results || responseData;
     } catch (error) {
         console.error("Fetch failed:", error);
     }
@@ -15,27 +15,47 @@ async function fetchPokemonFirstGeneration() {
 
 async function myRenderFunction() {
     let pokemonFirstGeneration = await fetchPokemonFirstGeneration();
-    console.log("Render function called");
-    console.log(pokemonFirstGeneration);
     let contentRef = document.getElementById("content");
-    for (let index = 0; index <= pokemonFirstGeneration.length; index++) {
-        console.log(pokemonFirstGeneration[index]);
-        contentRef.innerHTML += getMainPokedexTemplate(pokemonFirstGeneration[index].name);
-        
+    for (let i = 0; i < pokemonFirstGeneration.length; i++) {
+        let pokemon = pokemonFirstGeneration[i];
+
+        let detailResponse = await fetch(pokemon.url);
+        let detailData = await detailResponse.json();
+        // console.log(detailData);
+
+        let moveNames = detailData.moves.slice(0, 4).map(moveObj => moveObj.move.name);
+        let typeNames = detailData.types.slice(0, 1).map(typeObj => typeObj.type.name);
+        console.log(typeNames);
+
+
+        contentRef.innerHTML += getMainPokedexTemplate(pokemon.name, moveNames, typeNames);
+
     }
-
 };
 
-function getMainPokedexTemplate(name) {
-    return `<div>${name}</div>`
+// async function fetchAllPokemonTypes() {
+//     try {
+//         let response = await fetch("https://pokeapi.co/api/v2/type");
+//         let data = await response.json();
+//         console.log("Alle Pokémon-Typen:");
+//         console.table(data);
+//     } catch (error) {
+//         console.error("Fehler beim Laden der Typen:", error);
+//     }
+// }
+
+// fetchAllPokemonTypes();
+
+function getMainPokedexTemplate(name, moves, type) {
+    return `
+    <div>
+        <h3 class="${type}">${name}</h3>
+        <p>Moves:</p>
+        <ul>
+            ${moves}
+        </ul>
+    </div>`;
 };
+
 
 init();
-
-
-
-
-
-
-
-// POKEDEX API URL https://pokeapi.co/api/v2/pokemon?offset=0&limit=5
